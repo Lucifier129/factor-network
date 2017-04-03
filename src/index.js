0b7b315a741b6009a17930809ab2293446780536
工业聚 <guyingjie129@163.com>
2017-04-03 18:02:42 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,1 +1,202 @@
-console.log('placeholder')
+function createNetwork(options) {
+	let network = []
+	let previousInputs = options[0]
+	for (let i = 1; i < options.length; i++) {
+		let currentLayer = []
+		for (let j = 0; j < options[i]; j++) {
+			let currentNode = []
+			for (let k = 0; k < previousInputs; k++) {
+				let randomWeight = randomClamped()
+				currentNode.push(randomWeight)
+			}
+			currentLayer.push(currentNode)
+		}
+		previousInputs = currentLayer.length
+		network.push(currentLayer)
+	}
+	return network
+}
+
+function computeNetwork(network, inputs, activation) {
+	let currentInputs = inputs
+	let networkResult = []
+	for (let i = 0; i < network.length; i++) {
+		let currentLayer = network[i]
+		let currentLayerResult = []
+		for (let j = 0; j < currentLayer.length; j++) {
+			let currentNode = currentLayer[j]
+			let sum = 0
+			for (let k = 0; k < currentNode.length; k++) {
+				let currentWeight = currentNode[k]
+				sum += currentInputs[k] * currentWeight
+			}
+			let currentNodeResult = activation(sum)
+			currentLayerResult.push(currentNodeResult)
+		}
+		networkResult.push(currentLayerResult)
+		currentInputs = currentLayerResult
+	}
+	return networkResult
+}
+
+function walkNetwork(network, accessor) {
+	for (let i = 0; i < network.length; i++) {
+		let currentLayer = network[i]
+		for (let j = 0; j < currentLayer.length; j++) {
+			let currentNode = currentLayer[j]
+			for (let k = 0; k < currentNode.length; k++) {
+				let currentWeight = currentNode[k]
+				accessor({
+					weight: currentWeight,
+					node: currentNode,
+					layer: currentLayer,
+					network: network,
+					path: [i, j, k]
+				})
+			}
+		}
+	}
+}
+
+function mixNetwork(targetNetwork, sourceNetwork, mutation) {
+	walkNetwork(targetNetwork, function(data) {
+		if (randomBoolean()) {
+			let sourceWeight = sourceNetwork[data.path[0]][data.path[1]][data.path[2]]
+			if (Math.random() <= mutation.rate) {
+				sourceWeight += mutation.range * randomClamped()
+			}
+			targetNetwork[data.path[0]][data.path[1]][data.path[2]] = sourceWeight
+		}
+	})
+	return targetNetwork
+}
+
+function copyNetwork(network) {
+	return JSON.parse(JSON.stringify(network))
+}
+
+export default function createLearner(options) {
+	let networks = []
+
+	for (let i = 0; i < options.amount; i++) {
+		let network = createNetwork(options.network)
+		networks.push(network)
+	}
+
+	function getNetworks() {
+		return networks
+	}
+
+	function compute(index, inputs) {
+		return computeNetwork(
+			networks[index],
+			inputs,
+			activation[options.activation] || SIGMOID
+		) 
+	}
+
+	let sorts = []
+
+	function addItem(index, score) {
+		sorts.push({
+			score: score,
+			network: networks[index]
+		})
+	}
+
+	function clearAll() {
+		sorts = []
+	}
+
+	function sortItems(sortType) {
+		sorts.sort((a, b) => {
+			if (sortType > 0) {
+				return a.score - b.score
+			} else {
+				return b.score - a.score
+			}
+		})
+	}
+
+	function adjust() {
+		if (sorts.length !== options.amount) {
+			return
+		}
+
+		sortItems(options.sortType)
+
+		let newNetworks = []
+
+		let elitismAmount = Math.round(options.elitismRate * options.amount)
+		for (let i = 0; i < elitismAmount; i++) {
+			newNetworks.push(sorts[i].network)
+		}
+
+		let randomAmount = Math.round(options.randomRate * options.amount)
+		for (let i = 0; i < randomAmount; i++) {
+			newNetworks.push(createNetwork(options.network))
+		}
+
+		let mixCount = 0
+		while (true) {
+			for (let i = 0; i < mixCount; i++) {
+				for (let j = 0; j < options.mixNumber; j++) {
+					let newNetwork = mixNetwork(
+						copyNetwork(sorts[i].network),
+						sorts[mixCount].network,
+						options.mutation
+					)
+					newNetworks.push(newNetwork)
+					if (newNetworks.length === options.amount) {
+						sorts = []
+						networks = newNetworks
+						return
+					}
+				}
+			}
+			mixCount++
+		}
+	}
+
+	return {
+		getNetworks: getNetworks,
+		options: options,
+		compute: compute,
+		addItem: addItem,
+		clearAll: clearAll,
+		adjust: adjust
+	}
+}
+
+function randomClamped() {
+	return Math.random() * 2 - 1
+}
+
+function randomBoolean() {
+	return Math.random() <= 0.5
+}
+
+const activation = {
+	RELU: RELU,
+	SIGMOID: SIGMOID,
+	TANH: TANH,
+}
+
+function RELU(x) {
+	return Math.max(0, x)
+}
+
+function SIGMOID(x) {
+	return 1 / (1 + Math.exp(-x / 1))
+}
+
+function TANH(x) {
+	if (x === Infinity) {
+    return 1;
+  } else if (x === -Infinity) {
+    return -1;
+  } else {
+    let e2x = Math.exp(2 * x);
+    return (e2x - 1) / (e2x + 1);
+  }
+}
