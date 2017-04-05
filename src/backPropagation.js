d2ad8a24b682077e95e44dcb9120d1f95869c026
古映杰 <yj_gu@ctrip.com>
2017-04-05 19:40:05 +0800

    save

--- src/backPropagation.js
+++ src/backPropagation.js
@@ -5,14 +5,13 @@ function computeNetworkError(network, errors) {
 	let networkError = [errors.concat()]
 	let inputErrors = errors
 
-	for (let i = network.length - 2; i <= 0; i--) {
+	for (let i = network.length - 2; i >= 0; i--) {
 		let currentLayer = network[i]
 		let nextLayer = network[i + 1]
 		let layerError = []
-		for (let j = currentLayer.length - 1; j <= 0; j--) {
-			let currentNode = currentLayer[i]
+		for (let j = currentLayer.length - 1; j >= 0; j--) {
 			let nodeError = 0
-			for (let k = inputErrors.length - 1; k <= 0; k--) {
+			for (let k = nextLayer.length - 1; k >= 0; k--) {
 				let weight = nextLayer[k][j]
 				nodeError += inputErrors[k] * weight
 			}
@@ -29,10 +28,11 @@ function updateNetworkWeights(network, networkResult, networkError, activationTy
 	$network.walk(network, function(data) {
 		let path = data.path
 		let currentWeight = network[path[0]][path[1]][path[2]]
-		let currentInput = networkResult[path[0]][path[1]]
+		let currentInput = networkResult[path[0]][path[2]]
 		let currentResult = networkResult[path[0] + 1][path[1]]
 		let currentError = networkError[path[0]][path[1]]
-		let newWeight = currentWeight + learningRate * currentError * currentInput * activation[activationType].derivative(currentResult)
+		let deltaWeight = -learningRate * currentError * currentInput * activation[activationType].derivative(currentResult)
+		let newWeight = currentWeight + deltaWeight
 		network[path[0]][path[1]][path[2]] = newWeight
 	})
 }
@@ -40,32 +40,45 @@ function updateNetworkWeights(network, networkResult, networkError, activationTy
 export default function create(options) {
 	let network = $network.create(options.network)
 	let networkResult = null
-	let networkError = null
 
 	function getNetwork() {
 		return network
 	}
 
 	function compute(inputs) {
-		networkResult = $network.compute(network, inputs, options.activation || 'SIGMOID')
+		networkResult = $network.compute(
+			network,
+			inputs,
+			options.activation || 'SIGMOID'
+		)
 		return networkResult
 	}
 
-	function adjuest(labels) {
+	function computeError(labels) {
 		let errors = []
 		let lastResult = networkResult[networkResult.length - 1]
 		for (let i = 0; i < labels.length; i++) {
-			let error = labels[i] - lastResult[i]
+			let error = lastResult[i] - labels[i]
 			errors.push(error)
 		}
-		networkError = computeNetworkError(network, errors)
-		updateNetworkWeights(network, networkResult, networkError, options.activation, options.learningRate)
+		return computeNetworkError(network, errors)
+	}
+
+	function adjust(labels) {
+		let networkError = computeError(labels)
+		updateNetworkWeights(
+			network,
+			networkResult,
+			networkError,
+			options.activation || 'SIGMOID',
+			options.learningRate
+		)
 	}
 
 	return {
 		options: options,
 		getNetwork: getNetwork,
 		compute: compute,
-		adjuest: adjuest,
+		adjust: adjust,
 	}
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
+import * as network from './network'
 import backPropagation from './backPropagation'
 import evolution from './evolution'
 
 
 export default {
+	network: network,
 	backPropagation: backPropagation,
 	evolution: evolution,
 }
